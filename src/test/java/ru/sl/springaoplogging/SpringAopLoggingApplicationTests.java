491e638ae86d04d27b2eb27427b93355876c1c09
s.ladygin <s.ladygin@click-group.ru>
2023-10-03 11:23:56 +0300

    init commit

---
+++ src/test/java/ru/sl/springaoplogging/SpringAopLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sl.springaoplogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
